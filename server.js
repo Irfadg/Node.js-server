072a298213e654caeaaa4317f3534f4c40dcff75
Irfad <irfadg61@gmail.com>
2021-10-16 20:14:45 +0530

    user butten clicked respnse(quer.pathname)

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var reqst=require('http')
 var logPage=require('fs')
+var url=require('url')
 
 reqst.createServer(function (req,res){
-    if(req.url==='/'){
+    var query=url.parse(req.url)
+   // console.log(query.pathname)
+    if(query.pathname==='/'){
         logPage.readFile('main.html', function(err,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         })
-    }else if(req.url==='/sign-up'){
+    }else if(query.pathname==='/sign-up'){
         logPage.readFile('sign.html', function(err,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         })
+    }else if(query.pathname==='/buttoncliclked') {
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write("succesfully Data Stored")
+        res.end()
     }else {
         res.writeHead(404,{'Content-Type':'text/html'})
         res.write("page did't find!!!")
