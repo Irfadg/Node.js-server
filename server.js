6a4d539d07717c3fb0b887e71874408141ac1793
Irfad <irfadg61@gmail.com>
2021-10-16 23:39:10 +0530

    next ro learn expressjs

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ var logPage=require('fs')
 var url=require('url')
 
 reqst.createServer(function (req,res){
-    var query=url.parse(req.url)
+    var qr=url.parse(req.url,true)
    // console.log(query.pathname)
-    if(query.pathname==='/'){
+    if(qr.pathname==='/'){
         logPage.readFile('main.html', function(err,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         })
-    }else if(query.pathname==='/sign-up'){
+    }else if(qr.pathname==='/sign-up'){
         logPage.readFile('sign.html', function(err,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         })
-    }else if(query.pathname==='/buttoncliclked') {
+    }else if(qr.pathname==='/buttoncliclked') {
         res.writeHead(200,{'Content-Type':'text/html'})
+        console.log(qr.query.mail)
         res.write("succesfully Data Stored")
         res.end()
     }else {
